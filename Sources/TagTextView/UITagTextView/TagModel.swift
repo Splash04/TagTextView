25323f20d06c2291d8c9476a195845b79cf87ad0
Igor Kharitoniuk <igor.kharitoniuk@ventionteams.com>
2025-02-19 13:05:20 +0300

    Add public init to TagModel

--- Sources/TagTextView/UITagTextView/TagModel.swift
+++ Sources/TagTextView/UITagTextView/TagModel.swift
@@ -9,4 +9,13 @@ public struct TagModel: Identifiable {
     public var data: [AnyHashable: Any] = [:]
     public var isHashTag: Bool = false
     public var customTextAttributes: [NSAttributedString.Key: Any]?
+    
+    public init(id: String, name: String, range: NSRange, data: [AnyHashable : Any], isHashTag: Bool, customTextAttributes: [NSAttributedString.Key : Any]? = nil) {
+        self.id = id
+        self.name = name
+        self.range = range
+        self.data = data
+        self.isHashTag = isHashTag
+        self.customTextAttributes = customTextAttributes
+    }
 }
